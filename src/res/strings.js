e203dede0b5200e5f31d2c2c6164f03cfa39b80e
atoool <scrapycoco.sam@gmail.com>
2021-05-11 16:09:38 +0530

    1.3.52

--- src/res/strings.js
+++ src/res/strings.js
@@ -10,7 +10,7 @@ let strings = {
       ? 'https://apps.apple.com/in/app/fasting-tracker-intermittent/id1514726105'
       : `https://play.google.com/store/apps/details?id=${bundleId}`,
   api: 'https://forking.riafy.in/app-console/get-learn-series-api.php?page=home&type=home&appname=com.rstream.piano&lang=',
-  version: '1.1.51',
+  version: '1.3.52',
 };
 export default strings;
 // jump.rope.workout.counter
